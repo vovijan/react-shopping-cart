7dd52b5492a67b2f39a9a3a79e8d52dc7755e242
Vladimir Gusar <cancer@greendoubt.org>
2019-05-07 01:18:07 +0300

    ref sortfilter

--- src/functions/SortFilter.js
+++ src/functions/SortFilter.js
@@ -1,28 +1,30 @@
-export function applyFilter(data, company) {
+export const applyFilter = (data, company) => {
 	if (!company) return data;
 
 	return data.filter(item => item.company === company);
-}
+};
 
-export function getSortFunc(direction) {
+export const getSortFunc = (direction) => {
 	switch (direction) {
-		case 'aZ': return (first, second) => {
-			const nameA = first.name.toLowerCase();
-			const nameB = second.name.toLowerCase();
-			console.log(nameA);
-			if (nameA < nameB) {
-				return -1;
-			}
-			return 0;
-		};
-		case 'zA': return (first, second) => {
-			const nameA = first.name.toLowerCase();
-			const nameB = second.name.toLowerCase();
-			console.log(nameA);
-			if (nameA > nameB) {
-				return -1;
-			}
-			return 0;};
-		default: return () => 0;
+		case 'aZ':
+			return (first, second) => {
+				const nameA = first.name.toLowerCase();
+				const nameB = second.name.toLowerCase();
+				if (nameA < nameB) {
+					return -1;
+				}
+				return 0;
+			};
+		case 'zA':
+			return (first, second) => {
+				const nameA = first.name.toLowerCase();
+				const nameB = second.name.toLowerCase();
+				if (nameA > nameB) {
+					return -1;
+				}
+				return 0;
+			};
+		default:
+			return () => 0;
 	}
-}
+};
