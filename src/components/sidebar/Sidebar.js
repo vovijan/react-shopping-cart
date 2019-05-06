1c45c84fb355f89827945fc6e80c15d609fd1032
Vladimir Gusar <cancer@greendoubt.org>
2019-05-07 00:11:25 +0300

    refactoring filter sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,63 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchCompany from "./SearchCompany";
 
 import '../styleComponents.css';
 
-const Sidebar = (props) => {
-	return (
-		<div className="sidebar">
-			<div className="card border-secondary mb-3" style={{maxWidth: '25rem'}}>
-				<div className="card-header">Sort alphabetically</div>
-				<div className="btn-group-vertical" role="group" aria-label="Basic example">
-					<button
-						className="btn btn-light"
-						onClick={props.aZ}
-					>
-						A-Z
-					</button>
-					<button
-						className="btn btn-light"
-						onClick={props.zA}
-					>
-						Z-A
-					</button>
-				</div>
-			</div>
+export default class Sidebar extends Component {
+	state = {
+		isActive: false
+	};
+	handleClick = () => {
+		this.setState({
+			isActive: true
+		})
+	};
+	render() {
+		return (
+			<div className="sidebar">
+				<div className="card border-secondary mb-3" style={{maxWidth: '25rem'}}>
+					<div className="card-header">Company Name</div>
+					<div className="btn-group-vertical" role="group" aria-label="Basic example" onClick={this.handleClick}>
+						{
+							this.props.nameCompany.map((item) => {
+								return <SearchCompany
+									key={item.id}
+									names={item.company}
+									nameCompanyFilter={this.props.nameCompanyFilter}
 
-			<div className="card border-secondary mb-3" style={{maxWidth: '25rem'}}>
-				<div className="card-header">Company Name</div>
-				<div className="btn-group-vertical" role="group" aria-label="Basic example">
-					{
-						props.nameCompany.map((item) => {
-							return <SearchCompany
-											key={item.id}
-											names={item.company}
-											nameCompanyFilter={props.nameCompanyFilter}
-										/>
-						})
-					}
+								/>
+							})
+						}
+					</div>
 				</div>
-			</div>
 
-			<div className="card border-secondary mb-3" style={{maxWidth: '25rem'}}>
-				<div className="card-header">Stock</div>
-				<div className="btn-group-vertical" role="group" aria-label="Basic example">
-					<button
-						className="btn btn-light"
-						onClick={props.inStock}
-					>
-						In Stock
-					</button>
-					<button
-						className="btn btn-light"
-						onClick={props.outStock}
-					>
-						Out Stock
-					</button>
-				</div>
+				{
+					this.state.isActive ?
+						<>
+							<div className="card border-secondary mb-3" style={{maxWidth: '25rem'}}>
+								<div className="card-header">Sort alphabetically</div>
+								<div className="btn-group-vertical" role="group" aria-label="Basic example">
+									<button
+										className="btn btn-light"
+										onClick={this.props.aZ}
+									>
+										A-Z
+									</button>
+									<button
+										className="btn btn-light"
+										onClick={this.props.zA}
+									>
+										Z-A
+									</button>
+								</div>
+							</div>
+
+							<div className="card border-secondary mb-3" style={{maxWidth: '25rem'}}>
+								<div className="card-header">Stock</div>
+								<div className="btn-group-vertical" role="group" aria-label="Basic example">
+									<button
+										className="btn btn-light"
+										onClick={this.props.inStock}
+									>
+										In Stock
+									</button>
+									<button
+										className="btn btn-light"
+										onClick={this.props.outStock}
+									>
+										Out Stock
+									</button>
+								</div>
+							</div>
+						</> : null
+				}
 			</div>
-		</div>
-	)
+		)
+	}
 };
-
-export default Sidebar;
