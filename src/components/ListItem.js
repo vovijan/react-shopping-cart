69a2921068d59aab2ea884fb5f8b43e127a64b83
Vladimir Gusar <cancer@greendoubt.org>
2019-05-14 18:26:25 +0300

    add text center

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './styleComponents.css';
+
 export default class ListItem extends Component {
 
 	state = {
@@ -27,7 +29,7 @@ export default class ListItem extends Component {
 			<li>
 				<div className="card" style={{width: '18rem'}}>
 					<img src={pic} className="card-img-top" alt="Company product"/>
-					<div className="card-body">
+					<div className="card-body text-center">
 						<h6 className="card-title">-{name}-</h6>
 						<p className="card-text">{company}</p>
 						<p className="card-text" style={styleProd}>{color}</p>
